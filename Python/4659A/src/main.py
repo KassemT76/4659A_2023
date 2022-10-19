96161dfc232c968592d011ac993f5f9bd033848f
KassemT76 <kassemtaha06@gmail.com>
2022-10-19 15:46:59 -0400

    added Python actually this time

---
+++ Python/4659A/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       super                                                        #
+# 	Created:      2022-10-19, 3:46:18 p.m.                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
