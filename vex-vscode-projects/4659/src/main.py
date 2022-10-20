74dd49e1ed75da633c6e48ba2a50f978a9736a30
KassemT76 <kassemtaha06@gmail.com>
2022-10-20 15:16:19 -0400

    PYTHON FIXES

--- vex-vscode-projects/4659/src/main.py
+++ vex-vscode-projects/4659/src/main.py
@@ -11,27 +11,27 @@
 from vex import *
 
 # Brain should be defined by default
-Brain=Brain()
+brain=Brain()
 
 #DEFINING CONTROLLERS AND MOTORS
 Controller1 = Controller()
 #NUMBER IN BRACKETS IS PORT
-motor1 = Motor(1)
-motor2 = Motor(2)
-motor3 = Motor(3)
-motor4 = Motor(4)
+motor1 = Motor(4)
+motor2 = Motor(5)
+motor3 = Motor(10)
+motor4 = Motor(9)
 
 #ASSIGNING THE MOTORS TO THE CORRECT GROUPS
 motorsLeft = MotorGroup(motor1, motor2)
 motorsRight = MotorGroup(motor3, motor4)
 
 #TESTING PRINT
-Brain.screen.print("Program Loaded!")
+brain.screen.print("Program Loaded!")
 
 #IS CALLED WHEN AXIS2 (RIGHT JOYSTICK - VERTICAL) IS CHANGED
 def axisChanged2():
     #DEFINE POSITION OF CONTROLLER JOYSTICK
-    pos = Controller1.axis1.position()
+    pos = Controller1.axis2.position()
     #WHEN POS IS < 0 IT IS POINTING DOWN AND WE MOVE REVERSE
     if pos < 0:
         motorsRight.spin(REVERSE)
@@ -42,15 +42,19 @@ def axisChanged2():
 
 #IS CALLED WHEN AXIS3 (LEFT JOYSTICK - VERTICAL) IS CHANGED
 def axisChanged3():
-    pos = Controller1.axis3.position()
-    
-    if pos < 0:
+    #DEFINE POSITION OF CONTROLLER JOYSTICK
+    pos1 = Controller1.axis3.position()
+    #WHEN POS IS < 0 IT IS POINTING DOWN AND WE MOVE REVERSE
+    if pos1 < 0:
         motorsLeft.spin(REVERSE)
-        motorsLeft.set_velocity(pos, PERCENT)
+        motorsLeft.set_velocity(pos1, PERCENT)
+        brain.screen.print("Moving reverse left")
     else:
-        motorsRight.spin(FORWARD)
-        motorsLeft.set_velocity(pos, PERCENT)
+        motorsLeft.spin(FORWARD)
+        motorsLeft.set_velocity(pos1, PERCENT)
 
 #LISTENS FOR A CHANGE IN JOYSTICKS
-Controller1.axis2.changed(axisChanged2)
 Controller1.axis3.changed(axisChanged3)
+
+Controller1.axis2.changed(axisChanged2)
+
