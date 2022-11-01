e7ea8faa5b49e815ef9d5a7c00df744b94bfccb1
KassemT76 <kassemtaha06@gmail.com>
2022-11-01 18:01:36 -0400

    changed motor init, motor func, and added comptemp

--- vex-vscode-projects/4659/src/main.py
+++ vex-vscode-projects/4659/src/main.py
@@ -2,7 +2,7 @@
 #                                                                              #
 # 	Module:       main.py                                                      #
 # 	Author:       super                                                        #
-# 	Created:      2022-10-19, 3:46:18 p.m.                                       #
+# 	Created:      2022-10-19                                                   #
 # 	Description:  V5 project                                                   #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
@@ -15,11 +15,11 @@ brain=Brain()
 
 #DEFINING CONTROLLERS AND MOTORS
 Controller1 = Controller()
-#NUMBER IN BRACKETS IS PORT
-motor1 = Motor(Ports.PORT4, GearSetting.RATIO_6_1)
-motor2 = Motor(Ports.PORT5, GearSetting.RATIO_6_1)
-motor3 = Motor(Ports.PORT10, GearSetting.RATIO_6_1)
-motor4 = Motor(Ports.PORT9, GearSetting.RATIO_6_1)
+#INITIALIZING MOTORS
+motor1 = Motor(Ports.PORT2, GearSetting.RATIO_6_1, True)
+motor2 = Motor(Ports.PORT3, GearSetting.RATIO_6_1, True)
+motor3 = Motor(Ports.PORT7, GearSetting.RATIO_6_1, False)
+motor4 = Motor(Ports.PORT8, GearSetting.RATIO_6_1, False)
 
 #ASSIGNING THE MOTORS TO THE CORRECT GROUPS
 motorsLeft = MotorGroup(motor1, motor2)
@@ -29,43 +29,43 @@ motorsRight = MotorGroup(motor3, motor4)
 brain.screen.print("Program Loaded!")
 
 #IS CALLED WHEN AXIS2 (RIGHT JOYSTICK - VERTICAL) IS CHANGED
-def axisChanged2():
+def moveMRight():
     #DEFINE POSITION OF CONTROLLER JOYSTICK
     pos = Controller1.axis2.position()
     #WHEN POS IS < 0 IT IS POINTING DOWN AND WE MOVE REVERSE
     if pos < 0:
         motorsRight.spin(REVERSE)
-        #this should hopefully make the input less sensitive
-        if pos % 4 == 0:
+        if (pos%4)==0:
             motorsRight.set_velocity(pos, PERCENT)
     else:
         motorsRight.spin(FORWARD)
-        #this should hopefully make the input less sensitive
-        if pos % 4 == 0: 
+        if (pos%4)==0:
             motorsRight.set_velocity(pos, PERCENT)
 
 #IS CALLED WHEN AXIS3 (LEFT JOYSTICK - VERTICAL) IS CHANGED
-def axisChanged3():
+def moveMLeft():
     #DEFINE POSITION OF CONTROLLER JOYSTICK
     pos1 = Controller1.axis3.position()
     #WHEN POS IS < 0 IT IS POINTING DOWN AND WE MOVE REVERSE
     if pos1 < 0:
         motorsLeft.spin(REVERSE)
-        if pos1 % 4 == 0:
+        if (pos1 % 4) == 0:
             motorsLeft.set_velocity(pos1, PERCENT)
-        brain.screen.print("Moving reverse left")
     else:
         motorsLeft.spin(FORWARD)
-        if pos1 % 4 == 0:
+        if (pos1 % 4) == 0:
             motorsLeft.set_velocity(pos1, PERCENT)
 
+def driver():
+    #LISTENS FOR A CHANGE IN JOYSTICKS
+    Controller1.axis2.changed(moveMRight)
+    Controller1.axis3.changed(moveMLeft)
+    #BUTTON TO TEST AUTONUM IN DRIVE MODE
+    Controller1.buttonA.pressed(autonum)
+
 def autonum():
-         motorsLeft.spin_for(FORWARD, 1000, DEGREES, 100, RPM, wait = False)
-         motorsRight.spin_for(FORWARD, 1000, DEGREES, 100, RPM, wait = False)
+         motorsLeft.spin_for(REVERSE, 360, DEGREES, 300, RPM, wait = False)
+         motorsRight.spin_for(FORWARD, 360, DEGREES, 300, RPM, wait = False)
 
-#LISTENS FOR A CHANGE IN JOYSTICKS
-Controller1.axis2.changed(axisChanged2)
-Controller1.axis3.changed(axisChanged3)
-Controller1.buttonA.pressed(autonum)
-
-#Joseph was here
+#INITIALIZING COMPETITION MODE
+comp = Competition(driver, autonum)
