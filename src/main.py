7ef2311ebd047919ec9b58308c8dd9677502ec40
TheLightningSeal <gavinli2007123@gmail>
2022-11-03 20:16:15 -0400

    init

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       gavin                                                        #
+# 	Created:      2022-11-03, 8:08:19 p.m.                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
