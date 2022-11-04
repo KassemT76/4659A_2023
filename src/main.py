a020de8adf5fd076b7b1142fa4a1029fe60887a5
TheLightningSeal <gavinli2007123@gmail>
2022-11-03 20:57:35 -0400

    Config

--- src/main.py
+++ src/main.py
@@ -23,14 +23,15 @@ import numpy as np
 
 #Config----------------------------------------------------#
 brain          = vex.Brain()
+
 Flywheel1      = vex.Motor(vex.Ports.PORT11, vex.GearSetting.RATIO_36_1, True  )    #Direct Couple to internal motor shaft
 Flywheel2      = vex.Motor(vex.Ports.PORT12, vex.GearSetting.RATIO_36_1, False )
 Intake         = vex.Motor(vex.Ports.PORT13, vex.GearSetting.RATIO_36_1, True  )    #Not Finalized
 Rollers        = vex.Motor(vex.Ports.PORT21, vex.GearSetting.RATIO_18_1, False )    #Not Finalized
-LFMotor        = vex.Motor(vex.Ports.PORT15, vex.GearSetting.RATIO_6_1 , False )
-LRMotor        = vex.Motor(vex.Ports.PORT18, vex.GearSetting.RATIO_6_1 , False )
-RFMotor        = vex.Motor(vex.Ports.PORT19, vex.GearSetting.RATIO_6_1 , True  )
-RRMotor        = vex.Motor(vex.Ports.PORT20, vex.GearSetting.RATIO_6_1 , False )
+LFMotor        = vex.Motor(vex.Ports.PORT15, vex.GearSetting.RATIO_36_1 , False )
+LRMotor        = vex.Motor(vex.Ports.PORT18, vex.GearSetting.RATIO_36_1 , False )
+RFMotor        = vex.Motor(vex.Ports.PORT19, vex.GearSetting.RATIO_36_1 , True  )
+RRMotor        = vex.Motor(vex.Ports.PORT20, vex.GearSetting.RATIO_36_1 , False )
 
 
 #Motor Grouping---------------------------------------------------#
