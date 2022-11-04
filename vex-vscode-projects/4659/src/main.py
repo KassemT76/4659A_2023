bc2226e1a73df1184935dd7acfaffa0e9d029e28
KassemT76 <kassemtaha06@gmail.com>
2022-11-03 22:34:39 -0400

    Reorganized all variables and changed a whole lot

--- vex-vscode-projects/4659/src/main.py
+++ vex-vscode-projects/4659/src/main.py
@@ -8,32 +8,95 @@
 # ---------------------------------------------------------------------------- #
 
 # Library imports
-from vex import *
+import vex 
+import sys
 
-# Brain should be defined by default
-brain=Brain()
+#Config----------------------------------------------------#
+brain          = vex.Brain()
+Controller1    = vex.Controller()
 
-#DEFINING CONTROLLERS AND MOTORS
-Controller1 = Controller()
+Flywheel1      = vex.Motor(vex.Ports.PORT11, vex.GearSetting.RATIO_6_1  , True  )    #Do not change gear ratio
+Flywheel2      = vex.Motor(vex.Ports.PORT12, vex.GearSetting.RATIO_6_1  , False )    #Do not change gear ratio
+Intake         = vex.Motor(vex.Ports.PORT13, vex.GearSetting.RATIO_36_1 , True  )    #Not Finalized  
+Rollers        = vex.Motor(vex.Ports.PORT21, vex.GearSetting.RATIO_18_1 , False )    #Not Finalized
+LFMotor        = vex.Motor(vex.Ports.PORT15, vex.GearSetting.RATIO_36_1 , False )
+LRMotor        = vex.Motor(vex.Ports.PORT18, vex.GearSetting.RATIO_36_1 , False )
+RFMotor        = vex.Motor(vex.Ports.PORT19, vex.GearSetting.RATIO_36_1 , True  )
+RRMotor        = vex.Motor(vex.Ports.PORT20, vex.GearSetting.RATIO_36_1 , False )
 
-#INITIALIZING MOTORS
-motor1 = Motor(Ports.PORT2, GearSetting.RATIO_6_1, True)
-motor2 = Motor(Ports.PORT3, GearSetting.RATIO_6_1, True)
-motor3 = Motor(Ports.PORT7, GearSetting.RATIO_6_1, False)
-motor4 = Motor(Ports.PORT8, GearSetting.RATIO_6_1, False)
+#Program Internal Constants--------(Don't screw arround with this if you don't know what you are doing.)-----------#
+intakeStatus = False   #Switch for turning on and off intake. Set this variable to False in your code if u wanna switch it off.
+flywheelTargetRpm  = 3600       #The only thing that should touch this variable, is the flywheel control program, and the physics equation
 
+#DO NOT TOUCH U CAN DESTROY HARDWARE If you think this is an issue ask Gavin before changing stuff. (Gavin's Notes: Used for controlling startup and shutdown of flywheel)
+startUp      = False #False is flywheel startup not complete, True is complete
+Shutdown     = False #Used for shutting down flywheel
+startUpRPM   = 580
+internalRPM  = 0
+RPMIncrement = 10
+setSpeed = 2
 
-#ASSIGNING THE MOTORS TO THE CORRECT GROUPS
-motorsLeft = MotorGroup(motor1, motor2)
-motorsRight = MotorGroup(motor3, motor4)
+#Motor Grouping---------------------------------------------------#
+LHDrive  = vex.MotorGroup(LFMotor, LRMotor)
+RHDrive  = vex.MotorGroup(RFMotor, RRMotor)
+Flywheel = vex.MotorGroup(Flywheel1, Flywheel2)
 
-#TESTING PRINT
+#First print
 brain.screen.print("Program Loaded!")
-Thread.sleep_for(1000, MSEC)
-#INFORMATION HEADER
+#Information header
 brain.screen.clear_line()
 brain.screen.set_cursor(1,0)
 brain.screen.print("Information: ")
+
+#Low Level Services----------------------------------------------#
+
+#Figure out actual flywheel rpm
+def flywheelRPM():
+    GavinWasHere = Flywheel.velocity(vex.VelocityUnits.RPM) * 6
+    return(GavinWasHere)
+
+#Threading-------------------------------------------------------#
+def intakeControl():  #This is a intake control thread
+    while True:
+        if intakeStatus == True:
+            Intake.set_velocity(100, vex.PERCENT)
+
+        else: 
+            Intake.set_velocity(0  , vex.PERCENT)
+
+def flywheelControl(RPM):
+    if startUp == False:      #Turn on flywheel
+        global startUp
+        Flywheel.set_velocity((Flywheel.velocity(vex.VelocityUnits.RPM)), vex.VelocityUnits.RPM)
+        internalRPM = Flywheel.velocity(vex.VelocityUnits.RPM)
+        while internalRPM <= startUpRPM:
+            Flywheel.set_velocity(internalRPM, vex.VelocityUnits.RPM)
+            internalRPM = internalRPM + RPMIncrement
+        startUp = True
+
+#Driving Controls------------------------------------------------------------------#
+
+#Speed Settings-------------------------------------------#
+def changeSpeedDown():
+    global setSpeed
+    setSpeed = 2
+    Controller1.screen.clear_row(1)
+    Controller1.screen.set_cursor(1,0)
+    Controller1.screen.print("Speed",setSpeed)
+
+def changeSpeedN():
+    global setSpeed
+    setSpeed = 3
+    Controller1.screen.clear_row(1)
+    Controller1.screen.print("Speed",setSpeed)
+    
+def changeSpeedUp():
+    global setSpeed
+    setSpeed = 4
+    Controller1.screen.clear_row(1)
+    Controller1.screen.print("Speed",setSpeed)
+
+#Moving the motors----------------------------------------#
 
 #IS CALLED WHEN AXIS2 (RIGHT JOYSTICK - VERTICAL) IS CHANGED
 def moveMRight():
@@ -41,33 +104,38 @@ def moveMRight():
     pos = Controller1.axis2.position()
     #WHEN POS IS < 0 IT IS POINTING DOWN AND WE MOVE REVERSE
     if pos < 0:
-        motorsRight.spin(REVERSE)
-        motorsRight.set_velocity(pos, PERCENT)
+        RHDrive.spin(vex.REVERSE)
+        RHDrive.set_velocity((pos/4)*setSpeed, vex.PERCENT)
     else:
-        motorsRight.spin(FORWARD)
-        motorsRight.set_velocity(pos, PERCENT)
+        RHDrive.spin(vex.FORWARD)
+        RHDrive.set_velocity((pos/4)*setSpeed, vex.PERCENT)
     #PRINTS INFO
     brain.screen.set_cursor(2,0)
     brain.screen.clear_row()
-    brain.screen.print("Axis 2: ", Controller1.axis2.position(), "Velocity: ", motorsRight.velocity() )
+    brain.screen.print("Axis 2: ", Controller1.axis2.position(), "Velocity: ", RHDrive.velocity() )
    
 
 #IS CALLED WHEN AXIS3 (LEFT JOYSTICK - VERTICAL) IS CHANGED
 def moveMLeft():
+    global setSpeed
     #DEFINE POSITION OF CONTROLLER JOYSTICK
     pos1 = Controller1.axis3.position()
     #WHEN POS IS < 0 IT IS POINTING DOWN AND WE MOVE REVERSE
     if (pos1 < 0):
-        motorsLeft.spin(REVERSE)
-        motorsLeft.set_velocity(pos1, PERCENT)
+        LHDrive.spin(vex.REVERSE)
+        #if pos1%4 == 0
+        LHDrive.set_velocity((pos1/4)*setSpeed, vex.PERCENT)
     elif (pos1 >= 0):
-        motorsLeft.spin(FORWARD)
-        motorsLeft.set_velocity(pos1, PERCENT)
+        LHDrive.spin(vex.FORWARD)
+        LHDrive.set_velocity((pos1/4)*setSpeed, vex.PERCENT)
 
     #PRINTS INFO
     brain.screen.set_cursor(3,0)
     brain.screen.clear_row()
-    brain.screen.print("Axis 3 Changed: ", Controller1.axis3.position(),"Velocity: ", motorsLeft.velocity())
+    brain.screen.print("Axis 3 Changed: ", Controller1.axis3.position(),"Velocity: ", LHDrive.velocity())
+
+
+#Competition Templating----------------------------------------------------------------------------------#
 
 def driver():
     #LISTENS FOR A CHANGE IN JOYSTICKS
@@ -75,14 +143,18 @@ def driver():
     Controller1.axis3.changed(moveMLeft)
     #BUTTON TO TEST AUTONUM IN DRIVE MODE
     Controller1.buttonA.pressed(autonum)
-    wait(5, MSEC)
+    #ARROW BUTTONS TO CHANGE SPEED
+    Controller1.buttonDown.pressed(changeSpeedDown)
+    Controller1.buttonUp.pressed(changeSpeedUp)  
+    Controller1.buttonRight.pressed(changeSpeedN)
+    vex.wait(5)
 
 def autonum():
-         motorsLeft.spin_for(FORWARD, 360, DEGREES, 300, RPM, wait = False)
-         motorsRight.spin_for(FORWARD, 360, DEGREES, 300, RPM, wait = False)
+         LHDrive.spin_for(vex.FORWARD, 360, vex.DEGREES, 300, vex.RPM, wait = False)
+         RHDrive.spin_for(vex.FORWARD, 360, vex.DEGREES, 300, vex.RPM, wait = False)
 
 #INITIALIZING COMPETITION MODE
-comp = Competition(driver, autonum)
+comp = vex.Competition(driver, autonum)
 
 
     
